4a510df5893fbd4eb38678211a10c752ab00caa7
Daniel Jabaraj V <danieljabaraj218@gmail.com>
2024-10-10 12:17:34 +0530

    Add files via upload
    Displaying Text

---
+++ DisplayText.java
@@ -0,0 +1,9 @@
+public class DisplayText{
+    public static void main(String [] args){
+        System.out.println("Hello ✨"); //welcome message
+        System.out.println("Iam Daniel Jabaraj( DJ )"); //My Name
+        System.out.println("Iam a Beginner programmer 👨‍💻"); //who i am 
+        System.out.println("Eager to expand my knowledge 🙋‍♂️"); //habbit
+        System.out.println("My Email: danieljabaraj218@gmail.com"); //My email
+    }
+}
